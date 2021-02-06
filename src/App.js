d3e44c0e1e16f370f8c816d177eff1432047e66b
Romanovalm85@gmail.com <you@example.com>
2021-02-07 00:54:18 +0200

    hw-1.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // Модули
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 // Компоненты
 import Profile from './components/Profile/Profile';
@@ -23,6 +22,7 @@ const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />;
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
     </div>
